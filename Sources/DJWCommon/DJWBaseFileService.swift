95f2ac11abcab9eccbd77052cd4cbd2f20a90c9d
Trevor <trevor.d@beydag.com>
2020-09-08 19:47:03 +0100

    just an update

--- Sources/DJWCommon/DJWBaseFileService.swift
+++ Sources/DJWCommon/DJWBaseFileService.swift
@@ -24,7 +24,7 @@ open class DJWBaseFileService: NSObject {
         let fileManager = FileManager.default
         let dirPaths    = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
         let dURL        = dirPaths[0]
-        print("lazy var docsURL: (init here)")
+        //print("lazy var docsURL: (init here)")
         return dURL
     }()
     
